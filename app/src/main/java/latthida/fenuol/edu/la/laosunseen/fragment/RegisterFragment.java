56cc469e273a52e60b1757436edcb6cc3692e521
ninaksrs <ninaksrs@gmail.com>
2018-08-07 16:31:30 +0700

    Upload Image Successful

--- app/src/main/java/latthida/fenuol/edu/la/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/latthida/fenuol/edu/la/laosunseen/fragment/RegisterFragment.java
@@ -1,5 +1,9 @@
 package latthida.fenuol.edu.la.laosunseen.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,17 +12,67 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import latthida.fenuol.edu.la.laosunseen.MainActivity;
 import latthida.fenuol.edu.la.laosunseen.R;
 
 public class RegisterFragment extends Fragment{
 
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 //        Create Toolbar
+        createToolbar();
+
+//        Photo Controler
+        photoControler();
+
+
+
+    }   // Main Class
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 500, 650, true);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Photo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void photoControler() {
+        imageView = getView().findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"), 1);
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
@@ -31,8 +85,7 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-    }   // Main Class
+    }
 
     @Nullable
     @Override
